3acb79c683795922681c43dbbd7497d899be3943
Devesh Rathod <deveshrathod@Deveshs-MacBook-Air.local>
2024-07-24 15:43:31 +0530

    update

--- src/app.jsx
+++ src/app.jsx
@@ -4,5 +4,20 @@ import "./app.css";
 export function App() {
   const [count, setCount] = useState(0);
 
-  return <div>Suraj aahe chutiya</div>;
+  return (
+    <div>
+      <div>
+        Samveg ke kitne baap!!! {"->"} {count}
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +1
+        </button>
+      </div>
+    </div>
+  );
 }
